41255dbc0c6f2e10eba1eea25f1f4f7af82db790
Luis Moreno <luifer0115@gmail.com>
2023-07-28 02:31:09 -0500

    Update Connection string

--- BooksReviewAPI/Program.cs
+++ BooksReviewAPI/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-var connectioKey = builder.Configuration["ConnectionStrings:Default"];
+var connectioKey = builder.Configuration["ConnectionStrings"];
 
 var mySQLConfiguration = new MySQLConfiguration(connectioKey);
 builder.Services.AddSingleton(mySQLConfiguration);
@@ -17,12 +17,10 @@ builder.Services.AddScoped<IBookRepository, BookRepository>();
 
 var app = builder.Build();
 
-    // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
-    {
+   
         app.UseSwagger();
         app.UseSwaggerUI();
-    }
+    
 
 app.UseHttpsRedirection();
 
